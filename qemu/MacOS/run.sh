171d6c47fa2c56063aa557419b99ba1b8dd5c825
Brandon Yeh <lpopo0856@gmail.com>
2025-08-28 16:10:19 +0800

    [qemu] fix mac os script

--- qemu/MacOS/run.sh
+++ qemu/MacOS/run.sh
@@ -30,27 +30,25 @@ cp "$BREW_QEMU/share/qemu/edk2-x86_64-secure-code.fd" .
 
 # Create QCOW2 image if it doesn't exist
 if [ ! -f "./ff1.qcow2" ]; then
-    qemu-img create -f qcow2 ff1.qcow2 -o nocow=on 8G
+  qemu-img create -f qcow2 ff1.qcow2 -o nocow=on 8G
+  qemu-system-x86_64 \
+    -M q35 \
+    -accel tcg,thread=multi \
+    -cpu qemu64 -m 4G -smp 4 \
+    -device intel-hda -device hda-output \
+    -device qemu-xhci \
+    -drive if=pflash,format=raw,readonly=on,file="./edk2-x86_64-code.fd" \
+    -drive if=pflash,format=raw,readonly=on,file="./edk2-x86_64-secure-code.fd" \
+    -drive file=./ff1.qcow2,format=qcow2 \
+    -drive if=none,id=stick,format=raw,file="$ISO_FILE",readonly=on \
+    -device nec-usb-xhci,id=xhci                              \
+    -device usb-storage,bus=xhci.0,drive=stick \
+    -device virtio-gpu-pci \
+    -boot order=d,menu=on \
+    -display cocoa \
+    -netdev user,id=n1 \
+    -device virtio-net-pci,netdev=n1
 fi
-
-# Run QEMU with combined configuration
-qemu-system-x86_64 \
-  -M q35 \
-  -accel tcg,thread=multi \
-  -cpu qemu64 -m 4G -smp 4 \
-  -device intel-hda -device hda-output \
-  -device qemu-xhci \
-  -drive if=pflash,format=raw,readonly=on,file="./edk2-x86_64-code.fd" \
-  -drive if=pflash,format=raw,readonly=on,file="./edk2-x86_64-secure-code.fd" \
-  -drive file=./ff1.qcow2,format=qcow2 \
-  -drive if=none,id=stick,format=raw,file="$ISO_FILE",readonly=on \
-  -device nec-usb-xhci,id=xhci                              \
-  -device usb-storage,bus=xhci.0,drive=stick \
-  -device virtio-gpu-pci \
-  -boot order=d,menu=on \
-  -display cocoa \
-  -netdev user,id=n1 \
-  -device virtio-net-pci,netdev=n1
 
 qemu-system-x86_64 \
   -M q35 \
